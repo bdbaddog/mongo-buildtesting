9a8b3d83702f2c4a0fbb44763395db9e1fcdc37f
William Deegan <bill@baddogconsulting.com>
2018-08-21 12:20:40 -0400

    put log in parent dir

--- andrew_correctness.sh
+++ andrew_correctness.sh
@@ -10,8 +10,8 @@ git clean -xfd
 cp --verbose ../mongo/${path_to_nodes}/{__init__.py,FS.py} ./${path_to_nodes}/
 
 git checkout r3.7.1
-/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ./mongod 2>&1 | tee  BUILD.log.371.1 
+/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ./mongod 2>&1 | tee  ../BUILD.log.371.1 
 git checkout r3.7.2
-/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ./mongod 2>&1 | tee  BUILD.log.372.1
+/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ./mongod 2>&1 | tee  ../BUILD.log.372.1
 git checkout r3.7.1
-/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ./mongod 2>&1 | tee  BUILD.log.371.2
+/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ./mongod 2>&1 | tee  ../BUILD.log.371.2
