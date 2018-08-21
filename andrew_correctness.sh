60cc4df6c1918107292ff5410d34cff9b1b783ef
William Deegan <bill@baddogconsulting.com>
2018-08-21 14:51:54 -0400

    change log dir

--- andrew_correctness.sh
+++ andrew_correctness.sh
@@ -7,7 +7,8 @@ path_to_nodes=src/third_party/scons-2.5.0/scons-local-2.5.0/SCons/Node/
 cores=8
 
 flags="-j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= --debug=explain"
-
+log_dir=../logs
+mkdir -p ${log_dir}
 
 target=build/cached/mongo/base/global_initializer.os
 # target=./mongod
@@ -18,22 +19,22 @@ git clean -xfd
 cp --verbose ../mongo/${path_to_nodes}/{__init__.py,FS.py} ./${path_to_nodes}/
 
 git checkout r3.7.1
-/usr/bin/time python ./buildscripts/scons.py ${flags} ${target} 2>&1 | tee  ../BUILD.log.371.${revision}.1
+/usr/bin/time python ./buildscripts/scons.py ${flags} ${target} 2>&1 | tee  ${log_dir}/BUILD.log.371.${revision}.1
 
 # capture sconsign info
-python src/third_party/scons-2.5.0/sconsign.py -e global_initializer.os build/scons/sconsign.dblite > ../CSIG.${revision}.1
+python src/third_party/scons-2.5.0/sconsign.py -e global_initializer.os build/scons/sconsign.dblite > ${log_dir}/CSIG.${revision}.1
 
 git checkout r3.7.2
-/usr/bin/time python ./buildscripts/scons.py ${flags} ${target} 2>&1 | tee  ../BUILD.log.372.${revision}.1
+/usr/bin/time python ./buildscripts/scons.py ${flags} ${target} 2>&1 | tee  ${log_dir}/BUILD.log.372.${revision}.1
 
 # capture sconsign info
-python src/third_party/scons-2.5.0/sconsign.py -e global_initializer.os build/scons/sconsign.dblite > ../CSIG.${revision}.2
+python src/third_party/scons-2.5.0/sconsign.py -e global_initializer.os build/scons/sconsign.dblite > ${log_dir}/CSIG.${revision}.2
 
 git checkout r3.7.1
-/usr/bin/time python ./buildscripts/scons.py ${flags} ${target} 2>&1 | tee  ../BUILD.log.371.${revision}.2
+/usr/bin/time python ./buildscripts/scons.py ${flags} ${target} 2>&1 | tee  ${log_dir}/BUILD.log.371.${revision}.2
 
 # capture sconsign info
-python src/third_party/scons-2.5.0/sconsign.py -e global_initializer.os build/scons/sconsign.dblite > ../CSIG.${revision}.3
+python src/third_party/scons-2.5.0/sconsign.py -e global_initializer.os build/scons/sconsign.dblite > ${log_dir}/CSIG.${revision}.3
 
 # check results . Should be zero g++ -c's
 grep g++ ../BUILD.log.371.2 | grep -- "-c " | wc
