604301d8d5c01ec536eea9a3364bf9d558d190d7
William Deegan <bill@baddogconsulting.com>
2018-08-21 14:28:47 -0400

    fix target

--- andrew_correctness.sh
+++ andrew_correctness.sh
@@ -15,7 +15,7 @@ cp --verbose ../mongo/${path_to_nodes}/{__init__.py,FS.py} ./${path_to_nodes}/
 git checkout r3.7.1
 /usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ${target} 2>&1 | tee  ../BUILD.log.371.${revison}.1 
 git checkout r3.7.2
-/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= .${target} 2>&1 | tee  ../BUILD.log.372.${revison}.1
+/usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ${target} 2>&1 | tee  ../BUILD.log.372.${revison}.1
 git checkout r3.7.1
 /usr/bin/time python ./buildscripts/scons.py -j${cores} --ssl --link-model=dynamic --implicit-cache -j 12 --disable-warnings-as-errors --cache --build-fast-and-loose=on --modules= --variables-files= ${target} 2>&1 | tee  ../BUILD.log.371.${revison}.2
 
