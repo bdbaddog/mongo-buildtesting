0a0237804d8c151b7b0911ae4dc2b79f1ffb1318
William Deegan <bill@baddogconsulting.com>
2019-06-21 15:44:37 -0700

    Add log dir

--- retestit.sh
+++ retestit.sh
@@ -6,24 +6,20 @@ set -x
 END=10
 END=3
 
-#/usr/bin/time python buildscripts/scons.py -j 10 --variables-files=./etc/scons/mongodbtoolchain_stable_gcc.vars --ssl --implicit-cache --disable-warnings-as-errors --modules= --build-fast-and-loose=off --link-model=dynamic  2>&1 | tee BUILD.log
+#/usr/bin/time python buildscripts/scons.py -j 10 --variables-files=./etc/scons/mongodbtoolchain_stable_gcc.vars --ssl --implicit-cache --disable-warnings-as-errors --modules= --build-fast-and-loose=off --link-model=dynamic  2>&1 | tee ${LOGDIR}/BUILD.log
 
 for i in $(seq 1 $END); do
     echo $i;
-    /usr/bin/time python buildscripts/scons.py -j 10 --variables-files=./etc/scons/mongodbtoolchain_stable_gcc.vars --ssl --implicit-cache --disable-warnings-as-errors --modules= --build-fast-and-loose=on --link-model=dynamic  2>&1 | tee REBUILD.bfl.${i}
-    /usr/bin/time python buildscripts/scons.py -j 10 --variables-files=./etc/scons/mongodbtoolchain_stable_gcc.vars --ssl --implicit-cache --disable-warnings-as-errors --modules= --build-fast-and-loose=off --link-model=dynamic  2>&1 | tee REBUILD.nobfl.${i}
+    /usr/bin/time python buildscripts/scons.py -j 10 --variables-files=./etc/scons/mongodbtoolchain_stable_gcc.vars --ssl --implicit-cache --disable-warnings-as-errors --modules= --build-fast-and-loose=on --link-model=dynamic  --debug=explain 2>&1 | tee ${LOGDIR}/REBUILD.bfl.${i}
+    /usr/bin/time python buildscripts/scons.py -j 10 --variables-files=./etc/scons/mongodbtoolchain_stable_gcc.vars --ssl --implicit-cache --disable-warnings-as-errors --modules= --build-fast-and-loose=off --link-model=dynamic  --debug=explain 2>&1 | tee ${LOGDIR}/REBUILD.nobfl.${i}
 done
 
 
 echo "Build Fast Loose average"
-#grep user REBUILD.bfl.{?,??} |  sed 's/user.*//' | awk '{ total += $1; count++ } END { print total/count }'
-grep user REBUILD.bfl.{?,??} | sed 's/^[^:]*://g' | sed 's/user.*//' | awk '{ total += $1; count++ } END { print total/count }'
-
+grep user ${LOGDIR}/REBUILD.bfl.{?,??} | sed 's/^[^:]*://g' | sed 's/user.*//' | awk '{ total += $1; count++ } END { print total/count }'
 
 echo "No Build Fast Loose average"
-#grep user REBUILD.nobfl.{?,??} |  sed 's/user.*//' | awk '{ total += $1; count++ } END { print total/count }'
-grep user REBUILD.nobfl.{?,??} | sed 's/^[^:]*://g' | sed 's/user.*//' | awk '{ total += $1; count++ } END { print total/count }'
-
+grep user ${LOGDIR}/REBUILD.nobfl.{?,??} | sed 's/^[^:]*://g' | sed 's/user.*//' | awk '{ total += $1; count++ } END { print total/count }'
 
 
 
